90805463b7f489fab4333abe4852cd7776f59ea1
Wang Xue Rui <wangxuerui2003@gmail.com>
2023-07-15 17:56:26 +0800

    Add more user-friendly elements

--- generateNFTImages.py
+++ generateNFTImages.py
@@ -1,9 +1,10 @@
 import csv
 import os
 import sys
+import time
 from PIL import Image, ImageDraw, ImageFont
 import colorama
-from colorama import Fore, Back, Style
+from colorama import Fore
 colorama.init(autoreset=True)
 
 # Global constants
@@ -16,7 +17,6 @@ FONT_PATH = "./arlrdbd.ttf"
 FONT_SIZE = 140
 IMAGE_PADDING = 40
 LOGO_SIZE = 360
-
 
 
 # Global variables
@@ -40,6 +40,16 @@ except:
 	print(f"{Fore.LIGHTRED_EX}Font not found! Please put arlrdbd.ttf to the current working directory!")
 	sys.exit()
 
+print(f"{Fore.YELLOW}----- All images will be generated into the NFTImages/ folder -----\n{Fore.RESET}")
+
+mode = input("Choose mode (0 for generate all, 1 for ask before generate, q for quit): ")
+while mode != "0" and mode != "1" and mode != 'q':
+	print(f"{Fore.LIGHTRED_EX}Invalid mode!")
+	mode = input("Choose mode (0 for generate all, 1 for ask before generate): ")
+if mode == 'q':
+	print(f"{Fore.MAGENTA}Bye!")
+	sys.exit()
+mode = int(mode)
 
 
 def get_nft_info_list_from_csv(csv_path: str) -> list[dict]:
@@ -48,6 +58,8 @@ def get_nft_info_list_from_csv(csv_path: str) -> list[dict]:
 		and return a list of dictionaries containing the info
 	"""
 	nft_info_list: list[dict] = []
+
+	print(f"{Fore.LIGHTCYAN_EX}Loading data from csv file...", end='')
 
 	try:
 		with open(csv_path, 'r', encoding="utf-8", errors="ignore") as file:
@@ -60,9 +72,13 @@ def get_nft_info_list_from_csv(csv_path: str) -> list[dict]:
 					"nft_number_str": info.get("NFTNumber"),
 					"signature_bool": True if info.get("Signature") == "True" else False
 				}
-				nft_info_list.append(temp_dict)
+				if temp_dict.get("images_stack") and temp_dict.get("nft_number_str") and temp_dict.get("signature_bool"):
+					nft_info_list.append(temp_dict)
 	except IOError:
-		print(f"CSV file {csv_path} does not exist")
+		print(f"\n{Fore.LIGHTRED_EX}CSV file {csv_path} does not exist")
+		sys.exit()
+	
+	print(f"{Fore.LIGHTGREEN_EX} Done!")
 
 	return nft_info_list
 
@@ -71,6 +87,9 @@ def load_monkey_images(images_directory: str) -> dict:
 	"""
 		Load all possible images into a dict and sort them in the categories that they belongs to 
 	"""
+
+	print(f"{Fore.LIGHTCYAN_EX}Pre-loading all WuKong images into memory...", end='')
+
 	images_dict = {
 		"a": [],
 		"b": [],
@@ -82,7 +101,7 @@ def load_monkey_images(images_directory: str) -> dict:
 	}
 
 	if not os.path.exists(images_directory):
-		print(f"Directory {images_directory} does not exist!")
+		print(f"\nDirectory {images_directory} does not exist!")
 		sys.exit()
 
 	images = os.listdir(images_directory)
@@ -91,6 +110,8 @@ def load_monkey_images(images_directory: str) -> dict:
 		for key in images_dict.keys():
 			if image_path.startswith(f"wk{key}"):
 				images_dict[key].append(Image.open(f"{images_directory}/{image_path}").convert("RGBA"))
+
+	print(f"{Fore.LIGHTGREEN_EX} Done!")
 
 	return images_dict
 
@@ -108,8 +129,9 @@ def generate_nft_number_image(nft_number: str, width: int, height: int):
 
 
 def stack_image_layers(images: list, stack_numbers_list: list, signature: bool):
-	# for letter, number in zip(monkey_images_dict.keys(), stack_numbers_list):
-	# 	images.append(monkey_images_dict[letter][number - 1])
+	"""
+		Stack the image layers with a special logic
+	"""
 
 	images.append(monkey_images_dict['a'][stack_numbers_list[0] - 1])
 	images.append(monkey_images_dict['b'][stack_numbers_list[1] - 1])
@@ -144,13 +166,13 @@ def stack_image_layers(images: list, stack_numbers_list: list, signature: bool):
 	if stack_numbers_list[3] == 3:
 		images.append(monkey_images_dict['d'][stack_numbers_list[3] - 1])
 
-	if signature is True:
+	if signature is True: # stack signature if needed
 		images.append(signature_image)
 
 
 def generate_stacked_image(stack_numbers_list: int, signature: bool, nft_number: str, output_path: str):
 	"""
-		Generate the output image layer by layer
+		Generate the output image layer by layer and save to output_path
 	"""
 
 	images = []
@@ -158,22 +180,23 @@ def generate_stacked_image(stack_numbers_list: int, signature: bool, nft_number:
 
 	max_width = max(image.width for image in images)
 	max_height = max(image.height for image in images)
-	images.append(generate_nft_number_image(nft_number, max_width, max_height))
+	images.append(generate_nft_number_image(nft_number, max_width, max_height)) # append the "My Way xxxx" image
 
 	stacked_image = Image.new("RGBA", (max_width, max_height))
 
 	# stack the layers
 	for image in images:
-		x_offset = (max_width - image.width) // 2  # Calculate the x offset to center the image horizontally
-		y_offset = (max_height - image.height) // 2  # Calculate the y offset to center the image vertically
-		stacked_image.paste(image, (x_offset, y_offset), mask=image)
+		stacked_image.paste(image, (0, 0), mask=image)
 	
-	stacked_image.paste(logo_image, (IMAGE_PADDING, max_height - IMAGE_PADDING - logo_image.height - 15), mask=logo_image)
+	stacked_image.paste(logo_image, (IMAGE_PADDING, max_height - IMAGE_PADDING - logo_image.height - 15), mask=logo_image) # paste logo
 
-	stacked_image.save(output_path)
+	stacked_image.save(output_path) # save
 
 
 def put_images_to_folder(folder_name: str, nft_info_list: list[dict]):
+	"""
+		Generate NFT image one by one and put into the target folder
+	"""
 	if not os.path.exists(folder_name):
 		os.mkdir(folder_name)
 
@@ -181,10 +204,16 @@ def put_images_to_folder(folder_name: str, nft_info_list: list[dict]):
 		stack_numbers = list(map(int, nft_info["images_stack"].split(',')))
 		image_path = f"{folder_name}/{nft_info['nft_number_str'].zfill(4)}.png"
 		if os.path.exists(image_path):
-			print(f"{Fore.CYAN}{image_path} already exists, skipping...")
+			print(f"{image_path} already exists, skipping...")
 			continue
-		generate_stacked_image(stack_numbers, nft_info["signature_bool"], nft_info["nft_number_str"], image_path)
-		print(f"{Fore.LIGHTMAGENTA_EX}Generated {nft_info['nft_number_str']}.png!")
+		if mode == 1: # if in prompting mode
+			option = input(f"{Fore.RESET}Do you wish to generate {nft_info['nft_number_str']}.png? (N/Y, default Y): ")
+			if option.upper() == 'N':
+				return
+		print(f"{Fore.LIGHTMAGENTA_EX}Generating {nft_info['nft_number_str']}.png...", end='')
+		generate_stacked_image(stack_numbers, nft_info["signature_bool"], nft_info["nft_number_str"], image_path) # generate target image and save
+		print(f"{Fore.LIGHTGREEN_EX} Done!")
+
 
 
 def close_images():
@@ -198,13 +227,12 @@ def close_images():
 def main():
 	global nft_info_list, monkey_images_dict
 
-	print(f"{Fore.YELLOW}Initializing...")
 	nft_info_list = get_nft_info_list_from_csv(CSV_PATH)
 	monkey_images_dict = load_monkey_images(MONKEY_IMAGES_DIRECTORY)
-	print(f"{Fore.BLUE}Started generating...")
+	print(f"{Fore.BLUE}Started generating...{Fore.RESET}")
 	put_images_to_folder(OUTPUT_DIRECTORY, nft_info_list)
 	close_images()
-	print(f"{Fore.GREEN}Done!")
+	print(f"{Fore.GREEN}Finished generating all NFT images!")
 
 
 if __name__ == "__main__":
